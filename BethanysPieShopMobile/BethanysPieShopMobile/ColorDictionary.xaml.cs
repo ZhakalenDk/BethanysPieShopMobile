14a8c9ae2db41e51a200a7d156fe29204fc47a00
Mike Mortensen <mike2137@elevcampus.dk>
2022-08-19 11:08:21 +0200

    Fixed styles crashing the application

--- BethanysPieShopMobile/BethanysPieShopMobile/ColorDictionary.xaml.cs
+++ BethanysPieShopMobile/BethanysPieShopMobile/ColorDictionary.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace BethanysPieShopMobile.Styles
+namespace BethanysPieShopMobile
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ColorDictionary : ResourceDictionary
